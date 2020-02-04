a7e01100bfa850b90ada8962ba7259be8bb0e3fc
nawatend <32384185+nawatend@users.noreply.github.com>
2020-02-04 15:52:22 +0100

    dynamic cloud words en redesign person detail, beter responsive

--- client/src/pages/persons/personDetail.jsx
+++ client/src/pages/persons/personDetail.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PersonLayout from '../../layouts/personBase';
 import { useParams } from "react-router-dom";
-
+import TagCloud from 'react-tag-cloud'
 
 const PersonDetailPage = ({ match }) => {
 
+
+    let terms = ['lachen', 'hello', 'spannend', 'Noodle', 'opbsadsadeeun', 'kinding']
     let { person } = useParams()
     useEffect(() => {
         console.log(person)
@@ -18,19 +20,38 @@ const PersonDetailPage = ({ match }) => {
                     <div className="person__detail__image" style={{ backgroundImage: `url(/images/doctors/test2.jpg)` }}>
                     </div>
                     <div className="person__detail__text">
-                        Person Detail => {person} Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Quam odio consectetur, distinctio illo quo architecto laboriosam a quas quaerat facilis minus
-                         vitae labore. Ex maiores aperiam ducimus amet? Impedit, assumenda!
+                        Person Detail => {person} Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Quam  odio consectetur, distinctio illo quoodio consectetur, distinctio illo quo odio
+                         consectetur, distinctio illo quoodio consectetur, distinctio illo quoodio consectetur,
+                          distinctio illo quoodio consectetur, distinctio illo quo architecto laboriosam a quas
+                           quaerat facilis minus
+
                             </div>
-                    <div className="person__detail__title">ttt</div>
+                    <div className="person__detail__title">Dokter Man</div>
 
                     <div className="person__detail__mind">
                         <img src="/svgs/person/thinking.svg" alt="Thinking bubble" />
-                        <div className="mind__terms">
-                            <img src="/svgs/person/mind_terms.svg" alt="Thinking bubble main" />
-                            <div className="mind__terms--text">
-                                Text Text  Text Text  Text Text  Text Text   Text Text   Te Text Text   Text Text   Text Text   Text Text   Text Text  xt Text   Text Text   Text Text  Tex  Text Text  Text Text  Text Text  Text Text  Text Text  Text T ext  Text Text
-                            </div>
+
+                    </div>
+                    <div className="mind__terms" >
+                        <img src="/svgs/person/mind_terms.svg" alt="Thinking bubble main" />
+                        <div className="mind__terms--text">
+                            <TagCloud
+                                style={{
+                                    fontFamily: 'Laca',
+                                    fontWeight: 'bold',
+
+                                    color: 'ee7358',
+                                    padding: 4,
+                                    width: '100%',
+                                    height: '100%'
+                                }}>
+
+                                {terms.map((term, i) => (
+                                    <div key={terms + i} className={(i % 2 === 0) ? "cloud__word--l" : "cloud__word--s"}>{term}</div>
+                                ))}
+
+                            </TagCloud>
                         </div>
                     </div>
                 </div>
